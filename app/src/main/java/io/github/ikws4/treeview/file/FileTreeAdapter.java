829ac09e84d0f2af710c48e1ee68520681bc954c
asm_blahblah <aungsimin000@gmail.com>
2022-07-27 18:37:11 +0630

    Update implementation methods in demo codes along with the previous commit

--- app/src/main/java/io/github/ikws4/treeview/file/FileTreeAdapter.java
+++ app/src/main/java/io/github/ikws4/treeview/file/FileTreeAdapter.java
@@ -15,10 +15,11 @@ import io.github.ikws4.treeview.R;
 import io.github.ikws4.treeview.TreeItem;
 import io.github.ikws4.treeview.TreeView;
 
-class FileTreeAdapter extends TreeView.Adapter<FileTreeAdapter.ViewHolder, String> implements TreeView.Adapter.OnTreeItemClickListener<String> {
+class FileTreeAdapter extends TreeView.Adapter<FileTreeAdapter.ViewHolder, String> implements TreeView.Adapter.OnTreeItemClickListener<String>,TreeView.Adapter.OnTreeItemLongClickListener<String> {
 
   public FileTreeAdapter() {
     setTreeItemClickListener(this);
+    setTreeItemLongClickListener(this);
     File rootFile = Environment.getExternalStorageDirectory();
     TreeItem<String> root = new TreeItem<>(rootFile.getPath(), true);
     expand(root);
